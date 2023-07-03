a12e0807d42e82ac4853308562eb65898699852f
Baibhav Kumar <baibhav.kr73@gmail.com>
2023-07-03 23:22:49 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,56 +1,147 @@
 import { useEffect, useState } from "react";
+import { BsArrowLeftRight } from "react-icons/bs";
 
 export default function App() {
-  return <AllInOne />;
+  return (
+    <>
+      <AllInOne />
+      <CardBox />
+    </>
+  );
 }
 
 function AllInOne() {
+  const [data, setData] = useState({});
   const [amount, setAmount] = useState(1);
   const [baseCurr, setBaseCurr] = useState("USD");
   const [targetCurr, setTargetCurr] = useState("INR");
-  const [mainData, setMainData] = useState({});
-  const [optionData, setOptionData] = useState({});
+
   // console.log(mainData.conversion_rates);
-  const optionKeys = Object.keys(optionData);
+  const optionKeys = Object.keys(data);
   useEffect(
     function () {
       async function fetchApi() {
-        const res = await fetch(
-          `https://v6.exchangerate-api.com/v6/e3966fe3a6052bdda5c00bbc/latest/${baseCurr}`
-        );
-        const data = await res.json();
+        try {
+          const res = await fetch(
+            `https://v6.exchangerate-api.com/v6/e3966fe3a6052bdda5c00bbc/latest/${baseCurr}`
+          );
+          const data = await res.json();
+          setData(data.conversion_rates);
+        } catch (err) {}
         // console.log(data);
-        setMainData(data);
-        setOptionData(data.conversion_rates);
       }
       fetchApi();
+      // document.title = `${baseCurr} to ${targetCurr}`;
+      // return function () {
+      //   document.title = `Currency convertor`;
+      // };
     },
     [amount, baseCurr, targetCurr]
   );
   return (
     <>
       <div className="allInOne">
-        <input
-          type="text"
-          placeholder="Amount . ."
-          value={amount}
-          onChange={(e) => setAmount(Number(e.target.value))}
-        />
-        <select value={baseCurr} onChange={(e) => setBaseCurr(e.target.value)}>
-          {optionKeys.map((key) => (
-            <option>{key}</option>
-          ))}
-        </select>
-        <select
-          value={targetCurr}
-          onChange={(e) => setTargetCurr(e.target.value)}
-        >
-          {optionKeys.map((key) => (
-            <option>{key}</option>
-          ))}
-        </select>
-        <div>{(optionData[targetCurr] * amount).toFixed(2)}</div>
+        <fieldset>
+          <legend>Amount</legend>
+          <input
+            type="text"
+            placeholder=" Amount... "
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+          />
+        </fieldset>
+        <fieldset>
+          <legend>From</legend>
+          <select
+            value={baseCurr}
+            onChange={(e) => setBaseCurr(e.target.value)}
+          >
+            {optionKeys.map((key) => (
+              <option key={key}>{key}</option>
+            ))}
+          </select>
+        </fieldset>
+        <fieldset>
+          <legend>To</legend>
+          <select
+            value={targetCurr}
+            onChange={(e) => setTargetCurr(e.target.value)}
+          >
+            {optionKeys.map((key) => (
+              <option key={key}>{key}</option>
+            ))}
+          </select>
+        </fieldset>
+        <fieldset>
+          <legend>Result</legend>
+          <div>{(data[targetCurr] * amount).toFixed(2)}</div>
+        </fieldset>
       </div>
     </>
+  );
+}
+function CardBox() {
+  return (
+    <div className="cardBox">
+      <Card From={"USD"} To={"INR"} />
+      <Card From={"USD"} To={"EUR"} />
+      <Card From={"EUR"} To={"INR"} />
+    </div>
+  );
+}
+
+function Card({ From, To }) {
+  const [data, setData] = useState({});
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState(From);
+  const [to, setTo] = useState(To);
+  function swipe() {
+    setFrom(to);
+    setTo(from);
+  }
+  useEffect(function () {
+    document.title = `${from} to ${to}`;
+    return function () {
+      document.title = `Currency convertor`;
+    };
+  });
+  useEffect(
+    function () {
+      async function fetchApi() {
+        try {
+          const res = await fetch(
+            `https://v6.exchangerate-api.com/v6/e3966fe3a6052bdda5c00bbc/latest/${from}`
+          );
+          const data = await res.json();
+          setData(data.conversion_rates);
+        } catch (err) {}
+      }
+      fetchApi();
+    },
+    [amount, from, to]
+  );
+  return (
+    <div className="card">
+      <div className="border"></div>
+      <div className="top">
+        <fieldset>
+          <legend>Amount</legend>
+          <input
+            type="text"
+            placeholder=" Amount... "
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+          />
+        </fieldset>
+        <div className="from">{from}</div>
+        <div style={{ cursor: "pointer" }} onClick={swipe}>
+          <BsArrowLeftRight />
+        </div>
+        <div className="to">{to}</div>
+      </div>
+      <div className="bottom">
+        <h1>{(data[to] * amount).toFixed(4)}</h1>
+      </div>
+    </div>
   );
 }
